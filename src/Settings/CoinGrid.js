0dacda7e4cd4a615f22ff623f3d6d518349a5e97
Andrew Javidfar <javida1492@gmail.com>
2019-01-27 01:01:52 -0500

    Add CoinTile

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {AppContext} from '../App/AppProvider';
-import {Tile, SelectableTable} from "../Shared/Tile";
+//import {Tile, SelectableTile} from "../Shared/Tile";
+import CoinTile from "./CoinTile";
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     grid-gap: 15px;
+    margin-top: 40px;
 `
+
+
+function getCoinsToDisplay(coinList) {
+    return Object.keys(coinList).slice(0,100);
+}
+
 
 export default function () {
     return <AppContext.Consumer>
         {({coinList}) => <CoinGridStyled> 
             {Object.keys(coinList).map(coinKey => 
-                <SelectableTable> {coinKey} </SelectableTable>)}
+                <CoinTile coinKey={coinKey}/>)}
         </CoinGridStyled>}
     </AppContext.Consumer>
 }
--- src/Shared/Tile.js
+++ src/Shared/Tile.js
@@ -7,7 +7,7 @@ export const Tile = styled.div`
     padding: 10px;
 `
 
-export const SelectableTable = styled(Tile)`
+export const SelectableTile = styled(Tile)`
     &:hover {
         cursor: pointer;
         ${greenBoxShadow}
